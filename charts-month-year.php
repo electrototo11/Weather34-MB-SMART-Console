ea364a42df31c689bcf04708a5b87f9cf8d5f38a
brian underdown <brian260865@gmail.com>
2020-01-05 18:46:38 +0300

    MB SMART Standalone Version 1

---
+++ charts-month-year.php
@@ -0,0 +1,40 @@
+<?php include('livedata.php');
+	####################################################################################################
+	#	CREATED FOR HOMEWEATHERSTATION stand alone version											   #
+	# https://weather34.com/homeweatherstation/index.html 											   # 
+	# 	                                                                                               #
+	# 	Release: January 2020					  	                                                   #
+	# 	                                                                                               #
+	#   https://www.weather34.com 	                                                                   #
+	####################################################################################################
+// weather34 pure csv without mysql extras built September 2018 finished June 2019//
+date_default_timezone_set($TZ);
+$date = date_create();
+$year=date('Y');
+//disable error logging if you get server errors
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);ini_set("display_errors","off");
+
+
+//(date month-year-0)),MAX(Temp-1),MIN(Temp-2),MAX(Dewpoint-3),MIN(Dewpoint-4),TOTAL(Raintoday-5),MAX(Windspeed-6),AVG(Day WindSpeed-7),AVG(Temp-8),MAX(Barometer-9),MIN(Barometer-10)
+//create the current month file example folder/file path = weathercharts/2019/July.csv
+
+//path is weather34charts/year/month.csv (eg: weather34charts/2019/June.csv )
+$weatherchartfilemonth ="weather34charts/".$year."/".date('F').".csv";
+$weather34chartdata   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[32].",".$meteobridgeapi[158].",".$meteobridgeapi[123].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
+//$output=$weatherchartfilemonth;
+$fp = fopen($weatherchartfilemonth, 'a+'); 
+fwrite($fp,$weather34chartdata); 
+fclose($fp);
+
+//create the year file example folder/file path = weather34charts/2019.csv
+//(date month-year-0)),MAX(Temp-1),MIN(Temp-2),MAX(Dewpoint-3),MIN(Dewpoint-4),TOTAL(Raintoday-5),MAX(Windspeed-6),AVG(Day WindSpeed-7),AVG(Temp-8),MAX(Barometer-9),MIN(Barometer-10)
+//path is weather34charts/year.csv (eg: weathercharts/2019.csv )
+$weatherchartfileyear = "weather34charts/".date('Y').".csv";
+$weather34chartdata1   = date('j M').",".$meteobridgeapi[26].",".$meteobridgeapi[28].",".$meteobridgeapi[63].",".$meteobridgeapi[65].",".$meteobridgeapi[9].",".$meteobridgeapi[32].",".$meteobridgeapi[158].",".$meteobridgeapi[123].",".$meteobridgeapi[34].",".$meteobridgeapi[36].""."\r\n";
+//$output1=$weatherchartfileyear;
+$fp1 = fopen($weatherchartfileyear, 'a+'); 
+fwrite($fp1,$weather34chartdata1); 
+fclose($fp1);
+?>
+
+
---
+++ chartstoday.php
@@ -0,0 +1,26 @@
+<?php include('livedata.php');
+	####################################################################################################
+	#	CREATED FOR HOMEWEATHERSTATION stand alone version											   #
+	# https://weather34.com/homeweatherstation/index.html 											   # 
+	# 	                                                                                               #
+	# 	Release: January 2020					  	                                                   #
+	# 	                                                                                               #
+	#   https://www.weather34.com 	                                                                   #
+	####################################################################################################
+// weather34 pure csv without mysql extras built September 2018 finished June 2019//
+date_default_timezone_set($TZ);
+$date = date_create();
+$id=00;
+//disable error logging if you get server errors
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);ini_set("display_errors","off");
+//path is weather34charts/year/day.csv (eg: weathercharts/2019/24Jun2019.csv )
+$weatherchartfile = "weather34charts/".date('Y')."/".date('jMY').".csv";
+//ID-0,time(1),outsideTemp(2),barometer(3),raintoday(4),UV(5),windgustmph(6),windSpeedAvg(7),radiation(8),dewpoint(9),rainrate(10),direction(11),indoortemp(12),indoorhumidity(13),date(14),dewpoint(15),wetbulb(16),humidity(17),cloudbase(18)
+if ($meteobridgeapi[43]=='--'){$meteobridgeapi[43]=0;}
+if ($meteobridgeapi[45]=='--'){$meteobridgeapi[45]=0;}
+$weather34chartdata   = $id.",".date('H:i').",".$meteobridgeapi[2].",".$meteobridgeapi[10].",". $meteobridgeapi[9].",".$meteobridgeapi[43].",".$meteobridgeapi[40].",".$meteobridgeapi[17].",".$meteobridgeapi[45].",".$meteobridgeapi[4].",".$meteobridgeapi[8].",".$meteobridgeapi[7].",".$meteobridgeapi[22].",".$meteobridgeapi[23].",".date('j M').",".$weather['realfeel'].",".$weather['wetbulb'].",".$weather["humidity"].",".$weather['cloudbase'].""."\r\n";
+$output=$weatherchartfile;
+$fp = fopen($weatherchartfile, 'a+'); 
+fwrite($fp,$weather34chartdata); 
+fclose($fp);
+?>
