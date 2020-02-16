e8eee2318e109d83cf697c0b7514acf8eec74e92
brian underdown <brian260865@gmail.com>
2020-02-16 11:57:08 +0300

    MB SMART Standalone Version 3

--- weather34-webcam.php
+++ weather34-webcam.php
@@ -3,4 +3,6 @@
 <div class="modulecaptioncam">Web Cam</div>
 
 <!-- HOMEWEATHER STATION TEMPLATE SIMPLE WEBCAM -add your url as shown below do NOT delete the class='weather34-webcam' !!! -->
+<a href="weather34-large-cam.php" data-lity class='webcamlink'><?php echo $webcamicon?></a>
+
 <img src="<?php echo $webcamurl?>?v=<?php echo date('YmdGis');?>>" alt="weathercam" class="weather34-webcam">
