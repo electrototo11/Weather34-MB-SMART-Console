556c7b0eeebb59c6a6f0420f9fd8a7975953e954
brian underdown <brian260865@gmail.com>
2020-04-04 22:57:21 +0300

    Merge branch 'master' adds max min values to chart layouts

--- todaywindspeedmodule.php
+++ todaywindspeedmodule.php
@@ -2,4 +2,4 @@
  <?php include('livedata.php');?>
  <div class="modulecaptionchart">Wind Speed &nbsp;<blue1><?php echo $weather["wind_units"]; ?></blue1></div> 
  <iframe  class="charttempmodule" src="weather34charts/todaywindspeedmodulechart2.php" frameborder="0" scrolling="no" width="320px" height="250px"></iframe>  
- <div class="legendtemp">Max <?php echo $weather["wind_speed_max"] ." ". $weather["wind_units"]?></div>
+ <div class="legendtemp">Max <?php echo $weather["winddmax"] ." ". $weather["wind_units"]?></div> 
