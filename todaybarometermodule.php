6a9ed61817912d9162579a5c919a4e6f670c07c1
brian underdown <brian260865@gmail.com>
2020-04-05 14:35:04 +0300

    Merge branch 'master' adds max min values to chart layouts(fix firefox)…

--- todaybarometermodule.php
+++ todaybarometermodule.php
@@ -2,7 +2,7 @@
  <?php include('livedata.php');?>
  <div class="modulecaptionchart">Barometer &nbsp;<blue1><?php echo $weather["barometer_units"]; ?></blue1></div> 
  <iframe  class="charttempmodule" src="weather34charts/todaybarometermodulechart2.php" frameborder="0" scrolling="no" width="320px" height="250px"></iframe>  
-  <div class="legenddewpoint" style= "margin-left:-155px">Min <?php echo $weather["barometer_min"]?><?php echo $weather["barometer_units"]; ?></div>
+  <div class="legenddewpoint">Min <?php echo $weather["barometer_min"]?><?php echo $weather["barometer_units"]; ?></div>
  <div class="legendwetbulb" style="background-color:<?php 
  if ($weather["barometer_max"]<=5 ){echo '#4ba0ad';}
  else if ($weather["barometer_max"]<10 ){echo '#9bbc2f';}
