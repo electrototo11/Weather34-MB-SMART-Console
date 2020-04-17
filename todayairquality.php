e02082caa9b286d515f9c2f6ecf6a40912a7fe78
brian underdown <brian260865@gmail.com>
2020-04-17 21:44:22 +0300

    Merge branch 'master' adds Purple Air Daily Charts
    *note if purple air is yes the day chart for purple auto replaces the wind average chart.

--- todayairquality.php
+++ todayairquality.php
@@ -1,7 +1,7 @@
   
  <?php include('livedata.php');?>
 
- <div class="modulecaptionchartbaro"><?php echo $lang['Air Quality']?> &nbsp;</div> 
+ <div class="modulecaptionchartbaro"><?php echo $lang['Air Quality']?> &nbsp;<blue1>Purple Air</blue1></div> 
 
 
  <iframe  class="charttempmodule" src="weather34charts/todayairqualitychart.php" frameborder="0" scrolling="no" width="320px" height="250px"></iframe>  
